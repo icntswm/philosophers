4b91d9db1a94f3b5ba0638ffa4375f8c215fedf1
Frieda Kenned <fkenned@am-i1.msk.21-school.ru>
2021-08-13 18:07:17 +0300

    res++

--- philo.c
+++ philo.c
@@ -1,4 +1,5 @@
 #include "philo.h"
+#include <pthread.h> //compile with flag '-lpthread'
 
 void	struct_init(t_param *param)
 {
@@ -50,21 +51,17 @@ int main(int argc, char **argv)
 	error(argc);
 	struct_init(&param);
 	parser(argv, &param);
-	if (check_time() != param.time_start)
-		param.new_time = check_time() - param.time_start;
-	else
-		param.new_time = 0;
+	param.new_time = check_time() - param.time_start;
 	printf("new_time: %lld\n", param.new_time);
-	printf("number_of_philosophers: %d\n", param.number_of_philos);
-	printf("number_of_forks: %d\n", param.number_of_forks);
-	printf("time_to_die: %d\n", param.time_to_die);
-	printf("time_to_eat: %d\n", param.time_to_eat);
-	printf("time_to_sleep: %d\n", param.time_to_sleep);
-	printf("max_eat: %d\n", param.max_eat);
-	printf("old_time: %lld\n", param.old_time);
-	if (check_time() != param.time_start)
-		param.new_time = check_time() - param.time_start;
-	else
-		param.new_time = 0;
+	usleep(200000);
+	// printf("number_of_philosophers: %d\n", param.number_of_philos);
+	// printf("number_of_forks: %d\n", param.number_of_forks);
+	// printf("time_to_die: %d\n", param.time_to_die);
+	// printf("time_to_eat: %d\n", param.time_to_eat);
+	// printf("time_to_sleep: %d\n", param.time_to_sleep);
+	// printf("max_eat: %d\n", param.max_eat);
+	// printf("old_time: %lld\n", param.old_time);
+	param.new_time = check_time() - param.time_start;
 	printf("new_time: %lld\n", param.new_time);
+	
 }
